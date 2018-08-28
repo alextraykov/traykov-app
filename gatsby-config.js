feac2d0eaf041fced0083f14bb4f7032071833f9
alextraykov <alextraykov@gmail.com>
2018-08-28 10:43:57 +0300

    msg

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Alexander Traykov',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi, I'm Alexander Traykov</h1>
+    <p>I'm a product designer. Solving user problems, while building visually pleasing designs has been my main goal since the beginning of my career. I'm constantly searching for new paths that will expand my skill set.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+// test
+export default IndexPage
+
